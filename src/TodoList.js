d9e30d0940f6521740136dfe1a49340493415192
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2021-04-18 00:23:08 +0800

    handleinput change

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,46 @@
 import React, { Component, Fragment } from 'react';
 
 class TodoList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            handleinputValue:''
+        }
+    }
     render() {
         return(
             <Fragment>
                 <div>
-                    <input/>
-                    <button>提交</button>
+                    <input
+                        value = { this.state.inputValue }
+                        onChange = { (e) => this.handleInputChange(e) }
+                    />
+                    <button
+                        onClick = { (e) => this.handleBtnClick(e) }
+                    >提交</button>
                 </div>
-                <p>Hello</p>
+                <p>
+                    { this.state.handleinputValue }
+                </p>
             </Fragment>
         )
     }
+    handleInputChange(e) {
+        const inputvalue = e.target.value
+        if(inputvalue.length > 20) {
+            return
+        }
+        this.setState({
+            inputValue: inputvalue
+        })
+    }
+    handleBtnClick(e) {
+        this.setState({
+            handleinputValue: this.state.inputValue
+        })
+    }
 }
+
 
 export default TodoList;
